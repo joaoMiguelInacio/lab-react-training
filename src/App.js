27974337f90d9d8f02c4c6a0d8d386fec665aea7
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-06-17 17:37:46 +0100

    Iteration 8 done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import './App.css';
 
-//Iteration7
-import DriverCard from './components/DriverCard';
-//
+//Iteration8
+
+import LikeButton from './components/LikeButton';
+
 function App (){
     return (
         <>
-            <DriverCard
-                name="Travis Kalanick"
-                rating={4.2}
-                img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
-                car={{
-                    model: "Toyota Corolla Altis",
-                    licensePlate: "CO42DE"
-                }}
-            />
-
-            <DriverCard
-                name="Dara Khosrowshahi"
-                rating={4.9}
-                img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
-                car={{
-                    model: "Audi A3",
-                    licensePlate: "BE33ER"
-                }}
-            />  
+        <LikeButton />
+        <LikeButton />
         </>  
     )
 }
+
+//Iteration7
+// import DriverCard from './components/DriverCard';
+// 
+// function App (){
+//     return (
+//         <>
+//             <DriverCard
+//                 name="Travis Kalanick"
+//                 rating={4.2}
+//                 img="https://si.wsj.net/public/resources/images/BN-TY647_37gql_OR_20170621052140.jpg?width=620&height=428"
+//                 car={{
+//                     model: "Toyota Corolla Altis",
+//                     licensePlate: "CO42DE"
+//                 }}
+//             />
+//             <DriverCard
+//                 name="Dara Khosrowshahi"
+//                 rating={4.9}
+//                 img="https://ubernewsroomapi.10upcdn.com/wp-content/uploads/2017/09/Dara_ELT_Newsroom_1000px.jpg"
+//                 car={{
+//                     model: "Audi A3",
+//                     licensePlate: "BE33ER"
+//                 }}
+//             />  
+//         </>  
+//     )
+// }
 
 
 // Iteration6
