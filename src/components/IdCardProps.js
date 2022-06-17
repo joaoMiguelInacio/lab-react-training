80d89590c1e63db45ef6f69966fc9e9b5797e0d2
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-06-17 16:10:25 +0100

    props destructuring

--- src/components/IdCardProps.js
+++ src/components/IdCardProps.js
@@ -1,19 +1,20 @@
 function IdCardProps(props) {
+  const {isStudent, img, firstName, lastName, country} = props.data;
   return (
     <div
       className={
-        props.data.isStudent ? 'isStudent id-card' : 'isTeacher id-card'
+        isStudent ? 'isStudent id-card' : 'isTeacher id-card'
       }
     >
       <img
         className="picture"
-        src={props.data.img}
-        alt={props.data.firstName}
+        src={img}
+        alt={firstName}
       />
       <div className="data">
-        <p>Last Name: {props.data.lastName}</p>
-        <p>First Name: {props.data.firstName}</p>
-        <p>Country: {props.data.country}</p>
+        <p>Last Name: {lastName}</p>
+        <p>First Name: {firstName}</p>
+        <p>Country: {country}</p>
       </div>
     </div>
   );
