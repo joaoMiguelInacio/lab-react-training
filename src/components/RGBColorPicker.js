6f4ba3e174fb1f554e715164d0fd8a28b652e9f8
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-06-24 23:51:37 +0100

    Lab Solved - last iteration needs looking at

--- src/components/RGBColorPicker.js
+++ src/components/RGBColorPicker.js
@@ -1,33 +1,80 @@
 import { useState } from "react";
-import SingleColorPicker  from "./SingleColorPicker";
+import { Form, FormGroup, Label, Input} from 'reactstrap';
 
 export default function RGBColorPicker () {
     const [rValue, setRValue] = useState (0);
     const [gValue, setGValue] = useState (0);
     const [bValue, setBValue] = useState (0);
-    function handleSubmit (e) {
-        e.preventDefault();
-        console.log(e.target.value)
+    const [color, setColor] = useState("black");
+    const min = 0;
+    const max = 255;
+    function componentToHex(c) {
+        var hex = c.toString(16);
+        return hex.length == 1 ? "0" + hex : hex;
     }
+    function rgbToHex(rValue, gValue, bValue) {
+        return "#" + componentToHex(rValue) + componentToHex(gValue) + componentToHex(bValue);
+    }
+    function handleRChange (e) {
+        e.preventDefault();
+        const number = Math.max(min, Math.min(max, Number(e.target.value)));
+		setRValue(number);
+        const colorHex = rgbToHex(rValue, gValue, bValue) ;
+        setColor (colorHex);
+	};
+    function handleGChange (e) {
+        e.preventDefault();
+        const number = Math.max(min, Math.min(max, Number(e.target.value)));
+		setGValue(number);
+        const colorHex = rgbToHex(rValue, gValue, bValue) ;
+        setColor (colorHex);
+	};
+    function handleBChange (e) {
+        e.preventDefault();
+        const number = Math.max(min, Math.min(max, Number(e.target.value)));
+		setBValue(number);
+        const colorHex = rgbToHex(rValue, gValue, bValue) ;
+        setColor (colorHex);
+	};
     return (
         <>
-        <SingleColorPicker 
-            color="r"
-            value={rValue}
-            handleSubmit={handleSubmit}
-        />
-        <SingleColorPicker 
-            color="g"
-            value={gValue}
-            handleSubmit={handleSubmit}
-        />
-        <SingleColorPicker 
-            color="b"
-            value={bValue}
-            handleSubmi={handleSubmit}
-        />
+        <Form>
+            <FormGroup>
+                <Label for="rValue">R:</Label>
+                    <Input
+                        type="number"
+                        name="r"
+                        value={rValue}
+                        min="0"
+                        max="255"
+                        onChange={(e) => handleRChange(e)}
+                />
+            </FormGroup>
+            <FormGroup>
+                <Label for="gValue">G:</Label>
+                    <Input
+                        type="number"
+                        name="g"
+                        value={gValue}
+                        min="0"
+                        max="255"
+                        onChange={(e) => handleGChange(e)}
+                />
+            </FormGroup>
+            <FormGroup>
+                <Label for="bValue">B:</Label>
+                    <Input
+                        type="number"
+                        name="b"
+                        value={bValue}
+                        min="0"
+                        max="255"
+                        onChange={(e) => handleBChange(e)}
+                />
+            </FormGroup>
+        </Form>
         <div>
-            <div style={{backgroundColor : `rgb (${rValue}), (${gValue}), (${bValue})`, height:100, width:100, border: "solid black"}}></div>
+            <div style={{backgroundColor : `${color}`, height:100, width:100, border: `solid black`}}></div>
             <p>rgb ({rValue}, {gValue}, {bValue})</p>
         </div>
         </>
