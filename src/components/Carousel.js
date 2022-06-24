2c2eb06fe0cd0b1469240bc98cccb7637793e584
joaoMiguelInacio <joao.miguel.inacio@gmail.com>
2022-06-24 12:48:15 +0100

    carousel styling

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,7 +2,7 @@ import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 export default function Carousel({imageUrl, prevImage, nextImage}) {
     return (
-        <>
+        < div style={{display: "flex", alignItems: "center", position: "absolute", left: "50%", top: "50%", transform: "translate(-50%, -50%)"}} >
             <FaChevronLeft
                 onClick={prevImage}
             />
@@ -10,6 +10,6 @@ export default function Carousel({imageUrl, prevImage, nextImage}) {
             <FaChevronRight
                 onClick={nextImage}
             />
-        </>
+        </div>
     );
 }
